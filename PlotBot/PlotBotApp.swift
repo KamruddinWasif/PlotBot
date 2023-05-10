9332297d53d085fd1ed145520c750881c771f74a
Wasif Kamruddin <kamruddinwasif@gmail.com>
2023-05-11 00:19:19 +1000

    Initial Commit

---
+++ PlotBot/PlotBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlotBotApp.swift
+//  PlotBot
+//
+//  Created by Wasif Kamruddin on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PlotBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
