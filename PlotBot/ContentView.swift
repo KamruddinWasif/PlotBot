5db6bf41cf248e0a6a8cec5af20de050e954ce69
Wasif Kamruddin <kamruddinwasif@gmail.com>
2023-05-14 02:24:22 +1000

    new animations, design, and functionality imporved

--- PlotBot/ContentView.swift
+++ PlotBot/ContentView.swift
@@ -1,46 +1,63 @@
 import SwiftUI
 import Alamofire
+import UIKit
 
 struct ContentView: View {
     @State private var scriptText = ""
     @State private var isLoading = false
-    @State private var subheadingText = ""
-    
+
     var body: some View {
-        VStack {
-            Text("Welcome to PlotBot!")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding(.bottom, 10)
-            
-            TypewriterView(text: "An AI powered movie script generator. Create your story now...", speed: 0.1)
-                .padding(.bottom, 30)
-            
-            Button(action: generateScript) {
-                Text("Generate Movie Script")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(isLoading ? Color.gray : Color.blue)
-                    .cornerRadius(10)
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Welcome to PlotBot!")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .padding(.top, 50)
+                
+                TypewriterView(text: "An AI powered movie script generator.", speed: 0.1)
+                    .foregroundColor(Color.white)
+                    .padding(.bottom, 50)
+                
+                Button(action: generateScript) {
+                    Text("Generate Movie Script")
+                        .font(.headline)
+                        .foregroundColor(.black)
+                        .padding()
+                        .background(isLoading ? Color.gray : Color.green)
+                        .cornerRadius(10)
+                }
+                .disabled(isLoading)
+                
+                if isLoading {
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle(tint: .green))
+                        .scaleEffect(2)
+                }
+                
+                ScrollView {
+                    Text(scriptText)
+                        .padding()
+                        .background(Color.gray.opacity(0.5))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+                Button(action: {
+                    UIPasteboard.general.string = scriptText
+                }) {
+                    Image(systemName: "doc.on.doc")
+                        .font(.footnote)
+                        .foregroundColor(.black)
+                        .padding()
+                        .background(Color.green)
+                        .cornerRadius(10)
+                }
+                .padding(.top, 20)
             }
-            .disabled(isLoading)
-            
-            if isLoading {
-                ProgressView()
-                    .progressViewStyle(CircularProgressViewStyle(tint: .blue))
-                    .scaleEffect(2)
-            }
-            
-            ScrollView {
-                Text(scriptText)
-                    .padding()
-                    .background(Color.black)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            }
+            .padding()
         }
-        .padding()
     }
     
     private func generateScript() {
@@ -52,7 +69,7 @@ struct ContentView: View {
             "Which of these do you think would be most successful in the marketplace?",
             "Okay, please ask me ten questions that would help clarify our needs for this project.",
             "Please provide the best possible answers to these questions",
-            "Based on these answers please provide a compelling logline and outline for the movie along with a synopsis, structure, and detailed    character descriptions making sure this movie feels completely original and incorporates all of the elements that would make for a great film.",
+            "Based on these answers please provide a compelling logline and outline for the movie along with a synopsis, structure, and detailed character descriptions making sure this movie feels completely original and incorporates all of the elements that would make for a great film.",
             "Great now please please write the first entire scene of this movie including dialogue in proper movie script format.",
             "Okay thanks, now please assume the persona of a major successful Hollywood producer who is extremely critical and generally pessimistic and critique our movie overall and scene 1.",
             "Okay, based upon the feedback please add those elements and suggestions and rewrite scene 1 in movie script format.",
@@ -73,7 +90,7 @@ struct ContentView: View {
             }
         }
     }
-    
+
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()
@@ -91,7 +108,6 @@ struct ContentView: View {
                 .padding(.horizontal, 20)
                 .multilineTextAlignment(.center)
         }
-        
         private func typeText() {
             var currentCharacterIndex = 0
             Timer.scheduledTimer(withTimeInterval: speed, repeats: true) { timer in
@@ -114,3 +130,4 @@ struct ContentView: View {
         }
     }
 }
+
